8e7dbe83b0231434e6702ad79aa5db6827576a58
Aaron Worley <aaron@pikasoftware.com>
2016-07-26 20:33:34 -0400

    Changed match autofill from jQuery to PHP.  jQuery was tacking a duplicate option on the end, and had issues with the back button.

--- match_fields.php
+++ match_fields.php
@@ -1,9 +1,47 @@
 <?php
 
+function build_match_options($prefix, $columns, $selected)
+{
+	$z = '' . $selected;
+	
+	foreach ($columns as $value)
+	{
+		$y = $prefix . "." . $value;
+		
+		if ($y == $selected)
+		{
+			$z .= '<option selected>';
+		}
+		
+		else
+		{
+			$z .= '<option>';
+		}
+		
+		$z .= $y . '</option>';
+	}
+	
+	return $z;
+}
+
 class match_fields
 {
 	function run()
 	{
+		/*
+			I used the following SQL to build the first few variables.
+			
+			set session group_concat_max_len = 8192;
+			select group_concat(column_name) from information_schema.columns
+			where table_name = 'cases' and table_schema = 'cms';
+			select group_concat(column_name) from information_schema.columns
+			where table_name = 'contacts' and table_schema = 'cms';
+			select group_concat(column_name) from information_schema.columns
+			where table_name = 'activities' and table_schema = 'cms';
+		*/
+		$case_columns = array('case_id', 'number', 'client_id', 'user_id', 'cocounsel1', 'cocounsel2', 'office', 'problem', 'sp_problem', 'status', 'open_date', 'close_date', 'close_code', 'reject_code', 'poten_conflicts', 'conflicts', 'funding', 'undup', 'referred_by', 'intake_type', 'intake_user_id', 'last_changed', 'created', 'income', 'assets', 'poverty', 'income_type0', 'annual0', 'income_type1', 'annual1', 'income_type2', 'annual2', 'income_type3', 'annual3', 'income_type4', 'annual4', 'asset_type0', 'asset0', 'asset_type1', 'asset1', 'asset_type2', 'asset2', 'asset_type3', 'asset3', 'asset_type4', 'asset4', 'adults', 'children', 'persons_helped', 'citizen', 'citizen_check', 'client_age', 'outcome', 'outcome_notes', 'outcome_income_after_service', 'outcome_income_no_service', 'outcome_assets_after_service', 'outcome_assets_no_service', 'outcome_debt_after_service', 'outcome_debt_no_service', 'ca_outcome_amount_obtained', 'ca_outcome_monthly_obtained', 'ca_outcome_amount_reduced', 'ca_outcome_monthly_reduced', 'lsc_income_change', 'just_income', 'main_benefit', 'sex_assault', 'stalking', 'case_county', 'rural', 'good_story', 'case_zip', 'elig_notes', 'cause_action', 'lit_status', 'judge_name', 'court_name', 'court_address', 'court_address2', 'court_city', 'court_state', 'court_zip', 'docket_number', 'date_filed', 'protected', 'why_protected', 'pba_id1', 'pba_id2', 'pba_id3', 'referral_date', 'compensated', 'thank_you_sent', 'date_sent', 'payment_received', 'program_filed', 'dollars_okd', 'hours_okd', 'destroy_date', 'dom_viol', 'veteran_household', 'source_db', 'in_holding_pen', 'doc1', 'doc2', 'vawa_served');
+		$contact_columns = array('contact_id', 'first_name', 'middle_name', 'last_name', 'extra_name', 'alt_name', 'title', 'mp_first', 'mp_last', 'mp_alt', 'address', 'address2', 'city', 'state', 'zip', 'county', 'area_code', 'phone', 'phone_notes', 'area_code_alt', 'phone_alt', 'phone_notes_alt', 'email', 'org', 'birth_date', 'ssn', 'language', 'gender', 'ethnicity', 'notes', 'disabled', 'residence', 'marital', 'frail');
+		$note_columns = array('act_id', 'act_date', 'act_time', 'act_end_time', 'hours', 'completed', 'act_type', 'category', 'case_id', 'user_id', 'pba_id', 'funding', 'summary', 'notes', 'last_changed', 'created', 'last_changed_user_id', 'om_code', 'ph_measured', 'ph_estimated', 'estimate_notes', 'act_end_date', 'problem', 'location', 'media_items');
 		$lookup = array();
 		
 		if (!isset($_FILES['uploads']))
@@ -102,248 +140,35 @@ class match_fields
 ";
 		$i = 0;
 		foreach ($a2j_file->VARIABLES->VARIABLE as $val) 
-		{			
-			//print_r($val);
+		{
 			$a = $val->attributes();
 			// Remove spaces from the names of the HTML select elements.  Having
 			// names in them makes the jquery code ugly.
 			$menu_name = str_replace(" ", "_", $a['NAME']);
+			$selected = null;
+			
+			if (array_key_exists($menu_name, $lookup))
+			{
+				$selected = $lookup[$menu_name];
+			}
+			
 			echo "	
 			<tr><td>{$menu_name}</td>
 		<td>
 		</td><td>
 		<select class=\"form-control\" name=\"{$menu_name}\" id=\"match{$i}\">
-		<option selected></option>
-<option label>cases</option>
-<option>cases.case_id</option>
-<option>cases.number</option>
-<option>cases.client_id</option>
-<option>cases.user_id</option>
-<option>cases.cocounsel1</option>
-<option>cases.cocounsel2</option>
-<option>cases.office</option>
-<option>cases.problem</option>
-<option>cases.sp_problem</option>
-<option>cases.status</option>
-<option>cases.open_date</option>
-<option>cases.close_date</option>
-<option>cases.close_code</option>
-<option>cases.reject_code</option>
-<option>cases.poten_conflicts</option>
-<option>cases.conflicts</option>
-<option>cases.funding</option>
-<option>cases.undup</option>
-<option>cases.referred_by</option>
-<option>cases.intake_type</option>
-<option>cases.intake_user_id</option>
-<option>cases.last_changed</option>
-<option>cases.created</option>
-<option>cases.income</option>
-<option>cases.assets</option>
-<option>cases.poverty</option>
-<option>cases.income_type0</option>
-<option>cases.annual0</option>
-<option>cases.income_type1</option>
-<option>cases.annual1</option>
-<option>cases.income_type2</option>
-<option>cases.annual2</option>
-<option>cases.income_type3</option>
-<option>cases.annual3</option>
-<option>cases.income_type4</option>
-<option>cases.annual4</option>
-<option>cases.asset_type0</option>
-<option>cases.asset0</option>
-<option>cases.asset_type1</option>
-<option>cases.asset1</option>
-<option>cases.asset_type2</option>
-<option>cases.asset2</option>
-<option>cases.asset_type3</option>
-<option>cases.asset3</option>
-<option>cases.asset_type4</option>
-<option>cases.asset4</option>
-<option>cases.adults</option>
-<option>cases.children</option>
-<option>cases.persons_helped</option>
-<option>cases.citizen</option>
-<option>cases.citizen_check</option>
-<option>cases.client_age</option>
-<option>cases.outcome</option>
-<option>cases.lsc_income_change</option>
-<option>cases.just_income</option>
-<option>cases.main_benefit</option>
-<option>cases.sex_assault</option>
-<option>cases.stalking</option>
-<option>cases.case_county</option>
-<option>cases.rural</option>
-<option>cases.good_story</option>
-<option>cases.case_zip</option>
-<option>cases.elig_notes</option>
-<option>cases.cause_action</option>
-<option>cases.lit_status</option>
-<option>cases.judge_name</option>
-<option>cases.court_name</option>
-<option>cases.court_address</option>
-<option>cases.court_address2</option>
-<option>cases.court_city</option>
-<option>cases.court_state</option>
-<option>cases.court_zip</option>
-<option>cases.docket_number</option>
-<option>cases.date_filed</option>
-<option>cases.protected</option>
-<option>cases.why_protected</option>
-<option>cases.pba_id1</option>
-<option>cases.pba_id2</option>
-<option>cases.pba_id3</option>
-<option>cases.referral_date</option>
-<option>cases.compensated</option>
-<option>cases.thank_you_sent</option>
-<option>cases.date_sent</option>
-<option>cases.payment_received</option>
-<option>cases.program_filed</option>
-<option>cases.dollars_okd</option>
-<option>cases.hours_okd</option>
-<option>cases.destroy_date</option>
-<option>cases.dom_viol</option>
-<option>cases.veteran_household</option>
-<option>cases.source_db</option>
-<option>cases.in_holding_pen</option>
-<option>cases.doc1</option>
-<option>cases.doc2</option>
-<option>cases.vawa_served</option>
-<option>cases.litc_ci_1</option>
-<option>cases.litc_ci_2</option>
-<option>cases.litc_ci_3</option>
-<option>cases.litc_ci_4</option>
-<option>cases.litc_ci_5</option>
-<option>cases.litc_ci_6</option>
-<option>cases.litc_ci_7</option>
-<option>cases.litc_ci_8</option>
-<option>cases.litc_ci_9</option>
-<option>cases.litc_ci_10</option>
-<option>cases.litc_ci_11</option>
-<option>cases.litc_ci_12</option>
-<option>cases.litc_ci_13</option>
-<option>cases.litc_ci_14</option>
-<option>cases.litc_ci_15</option>
-<option>cases.litc_ci_16</option>
-<option>cases.litc_ci_17</option>
-<option>cases.litc_ci_18</option>
-<option>cases.litc_ci_19</option>
-<option>cases.litc_ci_20</option>
-<option>cases.litc_ci_21</option>
-<option>cases.litc_ci_22</option>
-<option>cases.litc_ci_23</option>
-<option>cases.litc_ci_24</option>
-<option>cases.litc_ci_25</option>
-<option>cases.litc_ci_26</option>
-<option>cases.litc_ci_27</option>
-<option>cases.litc_ci_28</option>
-<option>cases.litc_ci_29</option>
-<option>cases.litc_ci_30</option>
-<option>cases.litc_ci_31</option>
-<option>cases.litc_ci_32</option>
-<option>cases.litc_ci_33</option>
-<option>cases.litc_ci_34</option>
-<option>cases.litc_ci_35</option>
-<option>cases.litc_ci_36</option>
-<option>cases.litc_ci_37</option>
-<option>cases.litc_ci_38</option>
-<option>cases.litc_ci_39</option>
-<option>cases.litc_ci_40</option>
-<option>cases.litc_ci_41</option>
-<option>cases.litc_ci_42</option>
-<option>cases.litc_ci_43</option>
-<option>cases.litc_ci_44</option>
-<option>cases.litc_ci_45</option>
-<option>cases.litc_ci_46</option>
-<option>cases.litc_ci_47</option>
-<option>cases.litc_ci_48</option>
-<option>cases.litc_ci_49</option>
-<option>cases.litc_ci_50</option>
-<option>cases.litc_ci_51</option>
-<option>cases.litc_ci_52</option>
-<option>cases.litc_ci_53</option>
-<option>cases.litc_ci_54</option>
-<option>cases.litc_ci_55</option>
-<option>cases.litc_ci_56</option>
-<option>cases.litc_ci_57</option>
-<option>cases.litc_ci_58</option>
-<option>cases.litc_ci_59</option>
-<option>cases.litc_ci_60</option>
-<option>cases.litc_poa_irs</option>
-<option>cases.litc_irs_funct</option>
-<option>cases.litc_controversy_gt50k</option>
-<option>cases.litc_irs_matter</option>
-<option>cases.litc_tax_years_involved</option>
-<option>cases.litc_taxpayer_esl</option>
-<option>cases.litc_joint_rep</option>
-<option>cases.litc_rep_vol</option>
-<option>cases.litc_stm_handled</option>
-<option>cases.litc_taxpayer_compliance_filing</option>
-<option>cases.litc_taxpayer_compliance_collection</option>
-<option>cases.litc_refund_amount</option>
-<option>cases.litc_corrected_liabilities</option>
-<option>cases.google_drive_folder_id</option>
-<option label>contacts</option>
-<option>contacts.contact_id</option>
-<option>contacts.first_name</option>
-<option>contacts.middle_name</option>
-<option>contacts.last_name</option>
-<option>contacts.extra_name</option>
-<option>contacts.alt_name</option>
-<option>contacts.title</option>
-<option>contacts.mp_first</option>
-<option>contacts.mp_last</option>
-<option>contacts.mp_alt</option>
-<option>contacts.address</option>
-<option>contacts.address2</option>
-<option>contacts.city</option>
-<option>contacts.state</option>
-<option>contacts.zip</option>
-<option>contacts.county</option>
-<option>contacts.area_code</option>
-<option>contacts.phone</option>
-<option>contacts.phone_notes</option>
-<option>contacts.area_code_alt</option>
-<option>contacts.phone_alt</option>
-<option>contacts.phone_notes_alt</option>
-<option>contacts.email</option>
-<option>contacts.org</option>
-<option>contacts.birth_date</option>
-<option>contacts.ssn</option>
-<option>contacts.language</option>
-<option>contacts.gender</option>
-<option>contacts.ethnicity</option>
-<option>contacts.notes</option>
-<option>contacts.disabled</option>
-<option>contacts.residence</option>
-<option>contacts.marital</option>
-<option>contacts.frail</option>
-<option label>opposing party</option>
-<option>op.contact_id</option>
-<option>op.first_name</option>
-<option>op.middle_name</option>
-<option>op.last_name</option>
-<option>op.extra_name</option>
-<option>op.birth_date</option>
-<option label>opposing party attorney</option>
-<option>opa.contact_id</option>
-<option>opa.first_name</option>
-<option>opa.middle_name</option>
-<option>opa.last_name</option>
-<option>opa.extra_name</option>
-<option label>case notes</option>
-<option>notes.notes0</option>
-<option>notes.notes1</option>
-<option>notes.notes2</option>
-<option>notes.notes3</option>
-<option>notes.notes4</option>
-<option>notes.notes5</option>
-<option>notes.notes6</option>
-<option>notes.notes7</option>
-<option>notes.notes8</option>
-<option>notes.notes9</option>
-</select>
+		<option></option>
+<option label>cases</option>\n";
+echo build_match_options('cases', $case_columns, $selected);
+echo "<option label>contacts</option>\n";
+echo build_match_options('contacts', $contact_columns, $selected);
+echo "<option label>opposing party</option>\n";
+echo build_match_options('op', $contact_columns, $selected);
+echo "<option label>opposing party attorney</option>";
+echo build_match_options('opa', $contact_columns, $selected);
+echo "<option label>case notes</option>\n";
+echo build_match_options('notes', $note_columns, $selected);
+echo "</select>
 <script>
 $('#add').click(function(event){
 	event.preventDefault();
@@ -361,13 +186,6 @@ $('#add').click(function(event){
 </script>
 		</td></tr>
 ";
-
-		if (array_key_exists($menu_name, $lookup) && strlen($lookup[$menu_name]) > 0)
-		{
-			echo "<script>\n";
-			echo "$('#match" . $i . "').append(\"<option value='{$lookup[$menu_name]}' selected>{$lookup[$menu_name]}</option>\");\n";
-			echo "</script>\n";
-		}
 		
 		$i++;
 		}
