c1dfa4891d572b4fa596ecd7bab7dc4fd4b8d645
Aaron Worley <aaron@pikasoftware.com>
2016-08-26 14:06:08 -0400

    Made upload form language more readable.

--- upload_prompt.php
+++ upload_prompt.php
@@ -12,13 +12,17 @@ class upload_prompt
     <input type="text" class="form-control" id="url" name="url" placeholder="https://pikasoftware.com/legalservices/">
     <p class="help-block">This should be the same URL you enter in your web browser to log into your CMS.</p>
   </div>
-	<h2>Step 2:  Upload the Interview ZIP File</h2>
+	<h2>Step 2:  Upload the A2J interview source files</h2>
   <div class="form-group">
-	<p>Please upload the ZIP file for the interview that you want to publish.</p>
-	<input type="file" name="zip"><br>
-	<p class="help-block">This should be the ZIP file you downloaded from the A2J Author site.</p>
-	<p>Optionally, upload the last .PHP file you created.  CATS will load your previous settings from this file.</p>
-	<input type="file" name="previous_matches"><br>
+	<p>Please upload the ZIP file for the interview that you want to connect to your CMS.
+	This should be the ZIP file you downloaded from the A2J Author site.<br>
+	<input type="file" name="zip">
+	</p>
+	
+	<p>Optionally, upload a previously created A2J_ViewerAnswerSet.php file for this interview.  
+	CATS will load your previous settings from this file.<br>
+	<input type="file" name="previous_matches">
+	</p>
 	</div>
 	<input type="submit" class="btn btn-default btn-lg" value="Click Here to Upload and Continue to Step 3">
 	</form>
